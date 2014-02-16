c1567dd669567a4bb34dc7de0eabc1837296d6d2
streed <sean@cheapfit.me>
2014-02-16 03:11:58 -0500

    Finished the simple viewer.

--- antZoo/clusterView/app.py
+++ antZoo/clusterView/app.py
@@ -1,6 +1,12 @@
+import random
+import uuid
+
 from flask import Flask, render_template
 from flask.ext.wtf import Form
+from wtforms import TextField
+from wtforms.validators import Required
 
+from ..gossipService.gossiping.ttypes import GossipData
 from ..gossip import make_client
 
 app = Flask( __name__ )
@@ -11,9 +17,27 @@ nodes = [
     { "address": "localhost", "port": 33002 },
 ]
 
-@app.route( "/" )
+class KeyValueForm( Form ):
+    key = TextField( "Key", validators=[Required()] )
+    value = TextField( "Value", validators=[Required()] )
+
+@app.route( "/", methods=[ "GET", "POST" ] )
 def index():
-    return render_template( "index.html" )
+    form = KeyValueForm()
+
+    print form.validate_on_submit()
+
+    if( form.validate_on_submit() ):
+        key = form.key.data
+        value = form.value.data
+        
+        n = random.choice( nodes )
+
+        c = make_client( n["address"], n["port"] )
+
+        c.disseminate( GossipData( uuid=str( uuid.uuid4() ), key=key, value=value ) )
+    
+    return render_template( "index.html", form=form )
 
 @app.route( "/nodes" )
 def nodesView():
--- run_clusterview.py
+++ run_clusterview.py
@@ -1,4 +1,6 @@
 from antZoo.clusterView.app import app
 
 if __name__ == "__main__":
+    app.config["csrf_enabled"] = False
+    app.secret_key = "hello"
     app.run( debug=True )
