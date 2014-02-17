55f153c56f3af43cf46d634a7c3c79d67d1f99a0
streed <sean@cheapfit.me>
2014-02-17 02:56:20 -0500

    Added client timeout.

--- antZoo/gossip.py
+++ antZoo/gossip.py
@@ -21,6 +21,7 @@ logger = logging.getLogger( __name__ )
 
 def make_client( address, port ):
     transport = TSocket.TSocket( address, port )
+    transport.settimeout( 2000 )
     transport = TTransport.TBufferedTransport( transport )
     protocol = TBinaryProtocol.TBinaryProtocol( transport )
     client = Client( protocol )
