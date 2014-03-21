4cfc0967cef576ab5d6ddd0fff7d648e77739727
Sean Reed <sean@cheapfit.me>
2014-03-21 03:07:18 -0400

    Test runs two jobs and 200 tasks

--- test_scriptrunner.py
+++ test_scriptrunner.py
@@ -18,3 +18,14 @@ for i in range( 100 ):
 print "Done sending tasks."
 ant.finish()
 ant._runner.join()
+
+j = Job( "/Users/elchupa/code/school/antZoo/localenv/bin/python /Users/elchupa/code/school/antZoo/example_code/word_count.py" )
+
+ant.push( j )
+
+for i in range( 100 ):
+  ant.new_task( "this is another test that is to be run :D\n" )
+
+print "Done sending tasks."
+ant.finish()
+ant._runner.join()
